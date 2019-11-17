3f36b399f0f4e3e9abf71bb0ae72d615edc15753
Emanuel Kluge <mail@emanuel-kluge.de>
2019-11-17 20:30:41 +0100

    feat: link bot on startpage

--- src/get-index/app.js
+++ src/get-index/app.js
@@ -4,12 +4,17 @@ const path = require('path');
 
 const readFile = promisify(fs.readFile);
 
+const isProd = process.env.STAGE === 'prod';
+
 exports.getIndexHandler = async (event) => {
   console.log(event);
   const headers = { 'content-type': 'text/html; charset=utf8' };
   const content = await readFile(path.join(__dirname, 'index.html'), 'utf8');
   const currentYear = new Date().getFullYear();
-  const body = content.replace('{{year}}', currentYear);
+  const botName = isProd ? 'ek_tgif_bot' : 'ek_tgif_dev_bot';
+  const body = content
+    .replace('{{year}}', currentYear)
+    .replace('{{bot_name}}', botName);
 
   return {
     statusCode: 200,
