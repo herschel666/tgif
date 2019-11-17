57301b6895422e2ca82e441056e89b540bada2ba
Emanuel Kluge <mail@emanuel-kluge.de>
2019-11-17 19:59:05 +0100

    feat: add final start page

--- src/get-index/app.js
+++ src/get-index/app.js
@@ -1,16 +1,15 @@
+const { promisify } = require('util');
+const fs = require('fs');
+const path = require('path');
+
+const readFile = promisify(fs.readFile);
+
 exports.getIndexHandler = async (event) => {
   console.log(event);
   const headers = { 'content-type': 'text/html; charset=utf8' };
-  const body = `<!doctype html>
-<html>
-  <head lang="en">
-    <meta charset="utf-8">
-    <title>Hello World</title>
-  </head>
-  <body>
-    <h1>Hello World!</h1>
-  </body>
-</html>`;
+  const content = await readFile(path.join(__dirname, 'index.html'), 'utf8');
+  const currentYear = new Date().getFullYear();
+  const body = content.replace('{{year}}', currentYear);
 
   return {
     statusCode: 200,
