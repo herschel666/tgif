5d7929ca64bd0e2ee7f2cefc1219985726b747e8
Emanuel Kluge <mail@emanuel-kluge.de>
2019-11-17 19:39:28 +0100

    refactor: split code of routes

--- src/tgif/app.js
+++ src/tgif/app.js
@@ -12,24 +12,3 @@ exports.tgifHandler = async (event) => {
     text,
   });
 };
-
-exports.getIndexHandler = async (event) => {
-  console.log(event);
-  const headers = { 'content-type': 'text/html; charset=utf8' };
-  const body = `<!doctype html>
-<html>
-  <head lang="en">
-    <meta charset="utf-8">
-    <title>Hello World</title>
-  </head>
-  <body>
-    <h1>Hello World!</h1>
-  </body>
-</html>`;
-
-  return {
-    statusCode: 200,
-    headers,
-    body,
-  };
-};
