689d30fb6d52a11ed68b937adc99872405fc61cf
Emanuel Kluge <mail@emanuel-kluge.de>
2019-11-19 21:09:57 +0100

    feat: handle bot blocked by user gracefully

--- src/tgif/app.js
+++ src/tgif/app.js
@@ -4,11 +4,15 @@ exports.tgifHandler = async (event) => {
   console.log('Version: %s', process.env.GIT_SHA);
   console.log(event);
   const { message, edited_message } = JSON.parse(event.body);
-  const { text, chat, date, from } = { ...message, ...edited_message };
+  const { text, chat, date, from, message_id: messageId } = {
+    ...message,
+    ...edited_message,
+  };
 
   return await tgif({
     chatId: chat.id,
     fromId: from.id,
+    messageId,
     date,
     text,
   });
