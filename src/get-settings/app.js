6117bd13b5c8a100c8b660fafe3714a13169b279
herschel666 <kraemerseele@gmx.de>
2019-11-21 16:35:44 +0100

    refactor: add settings specific package to lib

--- src/get-settings/app.js
+++ src/get-settings/app.js
@@ -1,22 +1,9 @@
 const { promisify } = require('util');
 const fs = require('fs');
 const path = require('path');
+const { getValidatedParams } = require('@herschel666/tgif-settings');
 
 const readFile = promisify(fs.readFile);
-
-const getValidatedParams = ({ sessionId, ...pathParameters }) => {
-  const userId = parseInt(pathParameters.userId, 10);
-
-  if (Number.isNaN(userId)) {
-    throw Error('The given user ID is invalid.');
-  }
-
-  if (!Boolean(sessionId.match(/^[0-9a-f]{48}/))) {
-    throw Error('The given session ID is invalid.');
-  }
-
-  return { userId, sessionId };
-};
 
 exports.getSettingsHandler = async (event) => {
   console.log(event);
