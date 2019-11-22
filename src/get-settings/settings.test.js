6025ef89bf539aa4e2515b796ed215a1d5a47435
herschel666 <kraemerseele@gmx.de>
2019-11-22 15:38:05 +0100

    test(src/get-settings): check pre-population

--- src/get-settings/settings.test.js
+++ src/get-settings/settings.test.js
@@ -5,6 +5,7 @@ import { settings } from './settings';
 
 const FAKE_USER_ID = 1234567;
 const SESSION_ID = '1a2b3c4d5e6f7g8h9i0j';
+const TIMEZONE = 'Indian/Christmas';
 
 const test = ninos(ava);
 
@@ -47,19 +48,23 @@ test('valid session', async (t) => {
   const getSettingsSession = t.context.stub(async () => ({
     SessionId: SESSION_ID,
   }));
+  const getUser = t.context.stub();
   const result = await settings(
     {
       userId: FAKE_USER_ID,
       sessionId: SESSION_ID,
     },
-    { getSettingsSession }
+    { getSettingsSession, getUser }
   );
 
   t.is(getSettingsSession.calls.length, 1);
   t.is(getSettingsSession.calls[0].arguments[0], FAKE_USER_ID);
+  t.is(getUser.calls.length, 1);
+  t.is(getUser.calls[0].arguments[0], FAKE_USER_ID);
   t.is(result.statusCode, 200);
   t.true(result.headers['content-type'].includes('text/html'));
   t.true(result.body.includes('Edit your settings…'));
+  t.true(result.body.includes(`const userTimezone = '';`));
   t.false(
     result.body.includes('Successfully saved your settings.'),
     'No success message on initial page.'
@@ -74,20 +79,24 @@ test('invalid form submit', async (t) => {
   const getSettingsSession = t.context.stub(async () => ({
     SessionId: SESSION_ID,
   }));
+  const getUser = t.context.stub(async () => ({ Timezone: TIMEZONE }));
   const result = await settings(
     {
       userId: FAKE_USER_ID,
       sessionId: SESSION_ID,
       erroneous: true,
     },
-    { getSettingsSession }
+    { getSettingsSession, getUser }
   );
 
   t.is(getSettingsSession.calls.length, 1);
   t.is(getSettingsSession.calls[0].arguments[0], FAKE_USER_ID);
+  t.is(getUser.calls.length, 1);
+  t.is(getUser.calls[0].arguments[0], FAKE_USER_ID);
   t.is(result.statusCode, 200);
   t.true(result.headers['content-type'].includes('text/html'));
   t.true(result.body.includes('Edit your settings…'));
+  t.true(result.body.includes(`const userTimezone = '${TIMEZONE}';`));
   t.true(result.body.includes('Please select a timezone.'));
 });
 
