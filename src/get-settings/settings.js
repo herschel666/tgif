8c981e4c0d7d9a428d3170e8ea86c6898fa94167
herschel666 <kraemerseele@gmx.de>
2019-11-22 12:54:28 +0100

    refactor(src/get-settings): use new templates

--- src/get-settings/settings.js
+++ src/get-settings/settings.js
@@ -1,31 +1,22 @@
-const { promisify } = require('util');
-const fs = require('fs');
-const path = require('path');
-
-const readFile = promisify(fs.readFile);
+const expiredPage = require('./pages/expired');
+const formPage = require('./pages/form');
 
 const handler = async ({ userId, sessionId }, ddb) => {
   const sessionEntry = await ddb.getSettingsSession(userId);
+  const headers = { 'content-type': 'text/html; charset=utf8' };
 
   if (!sessionEntry || sessionEntry.SessionId !== sessionId) {
-    const headers = { 'content-type': 'text/plain; charset=utf8' };
-    const body = 'Session expired';
     return {
       statusCode: 403,
+      body: expiredPage,
       headers,
-      body,
     };
   }
 
-  const headers = { 'content-type': 'text/html; charset=utf8' };
-  const content = await readFile(path.join(__dirname, 'index.html'), 'utf8');
-  const currentYear = new Date().getFullYear();
-  const body = content.replace('{{year}}', currentYear);
-
   return {
     statusCode: 200,
+    body: formPage,
     headers,
-    body,
   };
 };
 
