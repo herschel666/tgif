e6f7610029ab528a622a46c66c845bf3d06d6756
herschel666 <kraemerseele@gmx.de>
2019-11-22 09:49:01 +0100

    feat(src/get-settings): verify session validity

--- src/get-settings/app.js
+++ src/get-settings/app.js
@@ -1,10 +1,11 @@
+const ddb = require('@herschel666/tgif-ddb');
+const { getValidatedParams } = require('@herschel666/tgif-settings');
 const { settings } = require('./settings');
 
-const { getValidatedParams } = require('@herschel666/tgif-settings');
-
 exports.getSettingsHandler = async (event) => {
+  console.log('Version: %s', process.env.GIT_SHA);
   console.log(event);
   const { userId, sessionId } = getValidatedParams(event.pathParameters);
 
-  return await settings({ userId, sessionId });
+  return await settings({ userId, sessionId }, ddb);
 };
