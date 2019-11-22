e3608eb031b624fa52faa8cbd3b168fd3de603df
herschel666 <kraemerseele@gmx.de>
2019-11-22 15:06:19 +0100

    refactor(src/get-settings): use validation helper

--- src/get-settings/settings.js
+++ src/get-settings/settings.js
@@ -1,12 +1,13 @@
+const { isValidSession } = require('@herschel666/tgif-settings');
 const expiredPage = require('./pages/expired');
 const successPage = require('./pages/success');
 const formPage = require('./pages/form');
 
 const handler = async ({ userId, sessionId, erroneous, successful }, ddb) => {
-  const sessionEntry = await ddb.getSettingsSession(userId);
+  const isSessionValid = await isValidSession({ userId, sessionId }, ddb);
   const headers = { 'content-type': 'text/html; charset=utf8' };
 
-  if (!sessionEntry || sessionEntry.SessionId !== sessionId) {
+  if (!isSessionValid) {
     return {
       statusCode: 403,
       body: expiredPage,
