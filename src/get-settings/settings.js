640b7a84270a62896bd94235e1e849ee6e259ddd
Emanuel Kluge <mail@emanuel-kluge.de>
2019-11-23 09:42:04 +0100

    fix(src/get-settings): sanitize timezone value
    ...before inserting it into the HTML

--- src/get-settings/settings.js
+++ src/get-settings/settings.js
@@ -1,4 +1,7 @@
-const { isValidSession } = require('@herschel666/tgif-settings');
+const {
+  isValidSession,
+  sanitizeTimezone,
+} = require('@herschel666/tgif-settings');
 const expiredPage = require('./pages/expired');
 const successPage = require('./pages/success');
 const formPage = require('./pages/form');
@@ -19,7 +22,7 @@ const handler = async ({ userId, sessionId, erroneous, successful }, ddb) => {
 
   if (!successful) {
     const user = await ddb.getUser(userId);
-    body = formPage(erroneous, user ? user.Timezone : '');
+    body = formPage(erroneous, user ? sanitizeTimezone(user.Timezone) : '');
   }
 
   return {
