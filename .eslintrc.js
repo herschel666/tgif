06d229cec8d05440c9efef2c199e49b421e01bf4
Emanuel Kluge <mail@emanuel-kluge.de>
2021-11-21 10:58:10 +0100

    chore: switch to new ESLint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   extends: ['plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 7,
+    requireConfigFile: false,
   },
   env: {
     node: true,
